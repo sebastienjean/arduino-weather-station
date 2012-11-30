53bef29c61886fdaeb352353da9b89e50441eda0
sebastienjean <baz.jean@gmail.com>
2012-11-30 11:28:36 +0100

    Initial commit
    -2 Eclipse projects, with build configurations for
    ATMega2560 (arduino Mega) and AT328P  (arduino Nano)
    * ArduinoCore project contains Arduino's core libraries, 
    taken from IDE v1.0.2
    * Weather-Station project contains for the moment only a "do
    nothing" arduino's application

---
+++ Weather-Station/main.cpp
@@ -0,0 +1,34 @@
+
+#include <Arduino.h>
+
+
+/**
+ * Application's main (what else to say?)
+ * @return (never)
+ */
+int main(void) {
+	init();
+
+	setup();
+
+	for (;;)
+		loop();
+
+	return 0;
+}
+/**
+ * Arduino's setup function, called once at startup, after init
+ */
+void setup() 
+{
+
+}
+
+/**
+ * Arduino's loop function, called in loop (incredible, isn't it ?)
+ */
+void loop() 
+{
+
+}
+
